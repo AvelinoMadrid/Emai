ee1e2e6f5e1f8ddf63a847bdb0d3cb1783e865db
Isay yamanki soriano <anubisio015@gmail.com>
2023-07-07 12:53:49 -0600

    cambios nuevos 3

---
+++ emai/Controllers/MenuGastos.cs
@@ -0,0 +1,29 @@
+﻿using emai.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace emai.Controllers
+{
+    public class Gastos : Controller
+    {
+        private readonly ILogger<Gastos> _logger;
+
+        public Gastos(ILogger<Gastos> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult MenuGastos()
+        {
+            return View();
+        }
+
+      
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
