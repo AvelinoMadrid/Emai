ff4b3d326d8aa8494324f99d9cb83aaf47ad7651
Isay yamanki soriano <anubisio015@gmail.com>
2023-07-17 15:52:36 -0600

    diseño y pantallas yama

---
+++ emai/Controllers/MenuHorarios.cs
@@ -0,0 +1,29 @@
+﻿using emai.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace emai.Controllers
+{
+    public class Horarios : Controller
+    {
+        private readonly ILogger<Horarios> _logger;
+
+        public Horarios(ILogger<Horarios> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult MenuHorarios()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
