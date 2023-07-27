9c30831b3394d0478341bafd425f06df423c256b
AvelinoMadrid <avelino_madrid@barcostudio.com>
2023-07-27 16:12:44 -0600

    cambios 27 julio

--- emai/Controllers/HorarioVeranoController.cs
+++ emai/Controllers/HorarioVeranoController.cs
@@ -14,40 +14,17 @@ namespace emai.Controllers
     public class HorarioVeranoController : Controller
     {
         private readonly IServicioHorarioVerano_Api _ServicioHorarioVeranoApi;
-        private readonly IServicioMaestro_Api _ServicioMaestroApi;
-        private readonly IServicioClase_Api _ServicioClaseApi;
 
-        public HorarioVeranoController(IServicioHorarioVerano_Api servicioHorarioVerano_Api, IServicioMaestro_Api servicioMaestro_Api, IServicioClase_Api servicioClase_Api)
+        public HorarioVeranoController(IServicioHorarioVerano_Api servicioHorarioVerano_Api)
         {
             string baseurl = "https://localhost:7265";
             _ServicioHorarioVeranoApi = servicioHorarioVerano_Api;
-            _ServicioMaestroApi = servicioMaestro_Api;
-            _ServicioClaseApi = servicioClase_Api;
         }
 
         public async Task<IActionResult> HorarioVerano()
         {
             List<HorarioVerano> Lista = await _ServicioHorarioVeranoApi.Lista();
 
-            var maestrosDisponibles = await _ServicioMaestroApi.ObtenerTodos();
-            var clasesDisponibles = await _ServicioClaseApi.ObtenerTodos();
-
-            foreach (var horarioVerano in Lista)
-            {
-                // Buscar el maestro correspondiente en la lista de MaestrosDisponibles por su ID
-                var maestro = maestrosDisponibles.FirstOrDefault(m => m.IdMaestro == horarioVerano.IdMaestro);
-                if (maestro != null)
-                {
-                    horarioVerano.NombreMaestro = maestro.Nombre;
-                }
-
-                // Buscar la clase correspondiente en la lista de ClasesDisponibles por su ID
-                var clase = clasesDisponibles.FirstOrDefault(c => c.idClase == horarioVerano.IdClase);
-                if (clase != null)
-                {
-                    horarioVerano.NombreClase = clase.Nombre;
-                }
-            }
             return View(Lista);
         }
 
@@ -61,9 +38,6 @@ namespace emai.Controllers
                 modelo_horarioVr = await _ServicioHorarioVeranoApi.Obtener(IdHorarioVerano);
                 ViewBag.Action = "Editar Horario Verano";
             }
-
-            modelo_horarioVr.MaestrosDisponibles = await _ServicioMaestroApi.ObtenerTodos();
-            modelo_horarioVr.ClasesDisponibles = await _ServicioClaseApi.ObtenerTodos();
 
             return View(modelo_horarioVr);
         }
