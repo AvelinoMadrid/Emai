33f87e5eb7ccfd6c17d7455ac3516d6deaa44680
DanielRamirezBarco <123116613+DanielRamirezBarco@users.noreply.github.com>
2024-01-03 10:26:55 -0600

    Cambios de loggeo

--- emai/Controllers/Login.cs
+++ emai/Controllers/Login.cs
@@ -1,11 +1,16 @@
 ﻿using emai.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using emai.Models;
+using Microsoft.Data.SqlClient;
+
 
 namespace emai.Controllers
 {
     public class Account : Controller
     {
+        public string EMAIConnection = "Data Source=LAPTOP-OM95FUOE\\SQLEXPRESS;Initial Catalog=EMAI;Integrated Security=True;TrustServerCertificate=True;";
+
         private readonly ILogger<HomeController> _logger;
 
         public Account(ILogger<HomeController> logger)
@@ -13,12 +18,46 @@ namespace emai.Controllers
             _logger = logger;
         }
 
-        public IActionResult Login()
+        public ActionResult Login()
         {
             return View();
         }
 
+       
 
-      
+
+        [HttpPost]
+        public ActionResult Login(Login user)
+        {
+            using (SqlConnection cn = new SqlConnection(EMAIConnection))
+            {
+                SqlCommand cmd = new SqlCommand("Loggeo", cn);
+                cmd.Parameters.Add(new SqlParameter("@Usuario", user.Usuario));
+                cmd.Parameters.Add(new SqlParameter("@Password", user.Contraseña));
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cn.Open();
+                
+                user.Usuario = cmd.ExecuteNonQuery().ToString();
+            }
+
+            if(user.Usuario != null)
+            {
+                //Session["usuario"] = user; 
+                
+                return RedirectToAction("Login", "Account");// nombre del metodo y controlador;<
+            }
+
+            else
+            {
+                ViewData["Mensaje"] = "Usuario no encontrado";
+                return View();
+            }
+
+
+           
+        }
+
+
+
     }
 }
---
+++ emai/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿namespace emai.Models
+{
+    public class Login
+    {
+        public int IdUsuario { get; set; }
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+    }
+}
