e49d65123a30a4c565ea18a14ef4c2cc449674db
Barcostudio <jose.barranco.acosta@gmail.com>
2024-01-26 23:31:42 -0600

    cambios DANIEL

--- EMAI.Comun/Models/PagosModel.cs
+++ EMAI.Comun/Models/PagosModel.cs
@@ -24,52 +24,52 @@ namespace EMAI.Comun.Models
         public decimal Subtotal { get; set; } 
         public decimal IVA { get; set; } 
         public decimal Total { get; set; } 
-
         public int IdAlumno { get; set; }
-
 
     }
 
     public class PagosIDModel
     {
         public int IdPago { get; set; }
-        public int IdPromosiones { get; set; }
-        public int IdAdicionales { get; set; }
-        public DateTime Fecha { get; set; }
         public string Folio { get; set; }
-        public DateTime FechaPago { get; set; }
-        public decimal SaldoPendiente { get; set; }
+        //public DateTime FechaPago { get; set; } 
+        public int IdPromosionales { get; set; }
         public string Mes { get; set; }
+        public int IdClasePago { get; set; }
         public int IdHorario { get; set; }
-        public string Dia { get; set; }
-        public int IdClase { get; set; }
-        public int IdRecepcionista { get; set; }
+        public string DiaAlumnoPago { get; set; }
+        public string InstrumentoAlimnoPago { get; set; }
+        public decimal CostoLibro { get; set; }
+        public string NombreLibro { get; set; }
+        //public string Atendio { get; set; } 
         public decimal Costo { get; set; }
-        public string Autorizacion { get; set; }
         public decimal Subtotal { get; set; }
-        public decimal Iva { get; set; }
+        public decimal IVA { get; set; }
         public decimal Total { get; set; }
+        public int IdAlumno { get; set; }
     }
 
     public class PagosInsertarModel
     {
-        public int IdPromosiones { get; set; }
-        public int IdAdicionales { get; set; }
-        public DateTime Fecha { get; set; }
+        
         public string Folio { get; set; }
-        public DateTime FechaPago { get; set; }
-        public decimal SaldoPendiente { get; set; }
+        //public DateTime FechaPago { get; set; } 
+        public int IdPromosionales { get; set; }
         public string Mes { get; set; }
+        public int IdClasePago { get; set; }
         public int IdHorario { get; set; }
-        public string Dia { get; set; }
-        public int IdClase { get; set; }
-        public int IdRecepcionista { get; set; }
+        public string DiaAlumnoPago { get; set; }
+        public string InstrumentoAlimnoPago { get; set; }
+        public decimal CostoLibro { get; set; }
+        public string NombreLibro { get; set; }
+        //public string Atendio { get; set; } 
         public decimal Costo { get; set; }
-        public string Autorizacion { get; set; }
         public decimal Subtotal { get; set; }
-        public decimal Iva { get; set; }
+        public decimal IVA { get; set; }
         public decimal Total { get; set; }
+        public int IdAlumno { get; set; }
     }
+}
 
     public class PagosActualizarModel
     {
@@ -112,4 +112,5 @@ namespace EMAI.Comun.Models
         public decimal Iva { get; set; }
         public decimal Total { get; set; }
     }
-}
+
+
