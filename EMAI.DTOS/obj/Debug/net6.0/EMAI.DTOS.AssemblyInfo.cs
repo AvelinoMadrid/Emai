ee2959fbed283408d804db61403e0252ba7054b5
pawer990392 <garcia9903.201.angel@gmail.com>
2024-03-23 23:43:38 -0600

    fuisionar

--- EMAI.DTOS/obj/Debug/net6.0/EMAI.DTOS.AssemblyInfo.cs
+++ EMAI.DTOS/obj/Debug/net6.0/EMAI.DTOS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMAI.DTOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+144d9963eef51356173668f744078b8fd42567f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d28f24c17a42144ebfb4bd5aa91ec112623e2045")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMAI.DTOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMAI.DTOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
